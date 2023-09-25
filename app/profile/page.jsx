ad67d1611f58d55b6faab0fd119932944b825482
Frekeasuquo <asuquondifreke06@gmail.com>
2023-09-25 20:20:28 +0100

    new features

--- app/profile/page.jsx
+++ app/profile/page.jsx
@@ -7,9 +7,10 @@ import { useRouter } from 'next/navigation'
 import Profile from '@components/profile'
 
 const MyProfile = () => {
+    const router = useRouter();
     const { data: session} = useSession();
 
-    const [ posts, setPosts] = useState([])
+    const [ posts, setPosts] = useState([])  
 
     useEffect(() => {
         const fetchPosts = async () => {
@@ -19,15 +20,29 @@ const MyProfile = () => {
             setPosts(data);
         }
 
-        if (session?.user.id)fetchPosts();
+        if (session?.user.id) fetchPosts();
     }, []);
 
-    const handleEdit = () => {
-
+    const handleEdit = (post) => {
+        router.push(`/update-prompt?id=${post._id}`)
     }
 
-    const handleDelete = async () => {
+    const handleDelete = async (post) => {
+        const hasConfirmed = confirm("Are you sure you want to delete this prompt?");
 
+        if(hasConfirmed) {
+            try {
+                await fetch(`/api/prompt/${post._id.toString()}`,{
+                    method: 'DELETE'
+                });
+
+                const filteredPosts = posts.filter((p) => p._id !== post._id)
+
+                setPosts(filteredPosts)
+            }   catch (error) {
+                console.log(error)
+            }
+        }
     }
 
     return (
@@ -36,7 +51,7 @@ const MyProfile = () => {
             desc= "Welcome to your personalized profile page"
             data= {posts}
             handleEdit={handleEdit}
-            handledelete= {handleDelete}
+            handleDelete= {handleDelete}
         />
     )
 }
--- components/PromptCard.jsx
+++ components/PromptCard.jsx
@@ -51,7 +51,7 @@ const PromptCard = ({ post, handleTagClick, handleEdit, handleDelete }) => {
 
             <p className="my-4 font-satoshi text-sm text-gray-700">{post.prompt}</p>
             <p className="font-inter text-sm blue_gradient cusror-pointer" onClick={ () => handleTagClick && handleTagClick(post.tag)}>
-                {post.tag}
+                #{post.tag}
             </p>
 
             {session?.user.id === post.creator._id && pathName === '/profile' && (
